24b02a26caf18ab32f79afb8e5a2e038a6f8de34
Ievgen Rybalko <jackrjack1@gmail.com>
2022-09-16 13:13:30 +0300

    Initial Commit

---
+++ BullsEyeUI/BullsEyeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeUIApp.swift
+//  BullsEyeUI
+//
+//  Created by Ievgen Rybalko on 16.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BullsEyeUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BullsEyeUI
+//
+//  Created by Ievgen Rybalko on 16.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
